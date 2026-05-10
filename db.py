e789d0f292b53d4a1370a76b46a0f38438c33957
DATTU <apple@Apples-MacBook-Air.local>
2026-05-10 16:55:05 +0530

    Fixed MongoDB SSL

--- db.py
+++ db.py
@@ -1,8 +1,13 @@
 from pymongo import MongoClient
+import certifi
 
 MONGO_URI = "mongodb+srv://vskoushal30_db_user:koushal%2B58@cluster0.6zmg3x9.mongodb.net/?appName=Cluster0"
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
 
 db = client["lifestyle_db"]
 
